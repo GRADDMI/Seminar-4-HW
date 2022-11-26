ffeaead53f993b7b717cb63530bca22b0d1378ba
Gradusov Dmitriy <graddmi@gmail.ru>
2022-11-26 23:18:03 +0300

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+Console.WriteLine("Введите последовательно 8 чисел:");
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+
+{
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write('[' + string.Join(", ", array) + ']');   
